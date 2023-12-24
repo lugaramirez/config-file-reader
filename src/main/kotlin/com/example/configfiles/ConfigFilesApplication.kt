0f7307a3dd56dd8b10681185ffc80fed5457fd4a
Gabriel Ramirez <luga.ramirez@gmail.com>
2023-12-24 15:04:57 +0100

    add an enum for the 'list' of files, just as PoC

--- src/main/kotlin/com/example/configfiles/ConfigFilesApplication.kt
+++ src/main/kotlin/com/example/configfiles/ConfigFilesApplication.kt
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.event.ApplicationStartedEvent
 import org.springframework.boot.runApplication
 import org.springframework.context.ApplicationListener
+import org.springframework.context.event.ContextRefreshedEvent
 import org.springframework.stereotype.Component
 import java.nio.file.Files
 import java.nio.file.Path
@@ -17,15 +18,22 @@ fun main(args: Array<String>) {
 }
 
 @Component
-class FileReader : ApplicationListener<ApplicationStartedEvent> {
+class FileReader : ApplicationListener<ContextRefreshedEvent> {
     private val log = KotlinLogging.logger { }
 
-    override fun onApplicationEvent(event: ApplicationStartedEvent) {
+    override fun onApplicationEvent(event: ContextRefreshedEvent) {
         try {
             log.info { "Application started with event: $event" }
-            log.info { Files.readString(Path.of("/config/something.yml")) }
+            Filenames.entries.forEach {
+                log.info { "\n${Files.readString(Path.of("/config/${it.filename}.yml"))}" }
+            }
         } catch (e: Exception) {
             log.error(e) { "Could not found file. Exception was: ${e.message}" }
         }
     }
+}
+
+enum class Filenames(val filename: String)  {
+    SOMETHING("something"),
+    ANOTHER("another"),
 }
